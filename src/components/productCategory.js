9b87230538aeef375d2aafd28188caf7b7c42ee7
N16H7H4WK-3R <aryan014kumar@gmail.com>
2024-02-23 21:08:05 +0530

    Assets Updates @stable

--- src/components/productCategory.js
+++ src/components/productCategory.js
@@ -1,31 +1,30 @@
+
 const callouts = [
-
-
     {
-        name: 'Desk and Office',
-        description: 'Work from home accessories',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        name: 'Mens',
+        description: 'Latest collection of casual wear.',
+        imageSrc: 'https://bollywoo.ooo/cdn/shop/products/off1.jpg?v=1681655209',
         imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
         href: '#',
     },
     {
-        name: 'Self-Improvement',
-        description: 'Journals and note-taking',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+        name: 'Women',
+        description: 'Focus on the latest fashion trends.',
+        imageSrc: 'https://filmfare.wwmindia.com/photogallery/2023/dec/triptidimri11702803514.jpg',
         imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
         href: '#',
     },
     {
-        name: 'Travel',
-        description: 'Daily commute essentials',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        name: 'Kids',
+        description: 'New arrivals are here.',
+        imageSrc: 'https://starsbiodata.com/wp-content/uploads/2017/06/Taimur-Ali-Khan.jpg',
         imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
         href: '#',
     },
     {
-        name: 'Travel',
-        description: 'Daily commute essentials',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+        name: 'Home & Decor',
+        description: 'Shop for the latest home decor products.',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
         imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
         href: '#',
     },
@@ -36,25 +35,25 @@ export default function Category() {
         <div className="bg-gray-900 pt-16 pb-2">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" style={{ borderTop: "1px solid white", borderBottom: "1px solid white" }}>
                 <div className="mx-auto max-w-2xl sm:py-8 lg:max-w-none">
-                    <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+                    <h2 className="text-2xl mb-8 font-bold text-white">Categories : </h2>
 
-                    <div className="mt-6 space-y-12 lg:grid lg:grid-cols-4 lg:gap-x-6 lg:space-y-0">
+                    <div className="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-12">
                         {callouts.map((callout) => (
                             <div key={callout.name} className="group relative">
-                                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                                     <img
                                         src={callout.imageSrc}
                                         alt={callout.imageAlt}
-                                        className="h-full w-full object-cover object-center"
+                                        className="h-80 w-full object-cover object-center group-hover:opacity-75"
                                     />
                                 </div>
-                                <h3 className="mt-6 text-sm text-gray-500">
+                                <h3 className="mt-6 text-sm text-white">
                                     <a href={callout.href}>
                                         <span className="absolute inset-0" />
                                         {callout.name}
                                     </a>
                                 </h3>
-                                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                                <p className="text-xs text-white">- {callout.description}</p>
                             </div>
                         ))}
                     </div>
--- src/components/productList.js
+++ src/components/productList.js
@@ -1,22 +1,20 @@
 
 const products = [
-
-
     {
         id: 1,
-        name: 'Earthen Bottle',
+        name: 'Throwback Hip Bag',
         href: '/over',
         price: '$48',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
-        imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
     },
     {
         id: 2,
-        name: 'Nomad Tumbler',
+        name: 'Medium Stuff Satchel',
         href: '/over',
         price: '$35',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
-        imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt: 'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
     },
     {
         id: 3,
@@ -34,15 +32,79 @@ const products = [
         imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
+    {
+        id: 5,
+        name: 'Throwback Hip Bag',
+        href: '/over',
+        price: '$48',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+    },
+    {
+        id: 6,
+        name: 'Medium Stuff Satchel',
+        href: '/over',
+        price: '$35',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt: 'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+    },
+    {
+        id: 7,
+        name: 'Focus Paper Refill',
+        href: '/over',
+        price: '$89',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+        id: 8,
+        name: 'Machined Mechanical Pencil',
+        href: '/over',
+        price: '$35',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+        id: 9,
+        name: 'Throwback Hip Bag',
+        href: '/over',
+        price: '$48',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+    },
+    {
+        id: 10,
+        name: 'Medium Stuff Satchel',
+        href: '/over',
+        price: '$35',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt: 'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+    },
+    {
+        id: 11,
+        name: 'Focus Paper Refill',
+        href: '/over',
+        price: '$89',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+        id: 12,
+        name: 'Machined Mechanical Pencil',
+        href: '/over',
+        price: '$35',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
 ]
 
 export default function List() {
     return (
-        <div className="bg-gray-900 pt-16">
+        <div className="bg-gray-900 pt-4">
             <div className="mx-auto max-w-2xl px-4 py-8 sm:px-4 sm:py-6 lg:max-w-7xl lg:px-8">
-                <h2 className="sr-only">Products</h2>
+                <h2 className="text-2xl mb-8 font-bold text-white">Trending in your region : </h2>
 
-                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <div className="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 xl:gap-x-12">
                     {products.map((product) => (
                         <a key={product.id} href={product.href} className="group">
                             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
@@ -52,8 +114,8 @@ export default function List() {
                                     className="h-full w-full object-cover object-center group-hover:opacity-75"
                                 />
                             </div>
-                            <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                            <h3 className="mt-4 text-xs text-white">{product.name}</h3>
+                            <p className="mt-1 text-xs font-medium text-white">{product.price}</p>
                         </a>
                     ))}
                 </div>
