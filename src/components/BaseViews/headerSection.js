26865c99e65547009e01e7255037b3a0ea3b32a1
N16H7H4WK-3R <aryan014kumar@gmail.com>
2024-02-28 21:35:27 +0530

    Refactor header to include welcome message

--- src/components/BaseViews/headerSection.js
+++ src/components/BaseViews/headerSection.js
@@ -1,13 +1,34 @@
-import logo from '/home/aryangupta/Personal_Space/Projects@2024/cosmo_craze/src/assets/cosmo_craze_transparent_logo.png';
-import Navigation from './navigationSection';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Navigation from './navigationSection';
+import axios from 'axios';
+import logo from '/home/aryangupta/Personal_Space/Projects@2024/cosmo_craze/src/assets/cosmo_craze_transparent_logo.png';
 
 export default function Header() {
     const navigate = useNavigate();
+    const [firstName, setFirstName] = useState(null);
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            axios.get('http://127.0.0.1:8000/user/fetch/data/', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                },
+            })
+                .then(response => {
+                    setFirstName(response.data.customer.first_name);
+                })
+                .catch(error => {
+                    console.error('Error fetching user information:', error);
+                });
+        }
+    }, []);
+
     return (
         <>
             <header className="bg-black sticky top-0 z-10">
-                <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
+                <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-4" aria-label="Global">
                     <div className="flex items-center">
                         <a href="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
@@ -20,9 +41,17 @@ export default function Header() {
                         </form>
                     </div>
                     <div className="flex lg:flex-1 lg:justify-end">
-                        <button onClick={() => navigate('/signin')} className="text-sm font-semibold leading-6 text-white hover:text-red-800">
-                            Log in <span aria-hidden="true">&rarr;</span>
-                        </button>
+                        {firstName ? (
+                            <div className="flex items-center">
+                                <button onClick={() => navigate('/profile')} className="text-xs font-semibold leading-6 text-white hover:text-red-800">
+                                    Welcome, {firstName}
+                                </button>
+                            </div>
+                        ) : (
+                            <button onClick={() => navigate('/signin')} className="text-sm font-semibold leading-6 text-white hover:text-red-800">
+                                Log in <span aria-hidden="true">&rarr;</span>
+                            </button>
+                        )}
                     </div>
                 </nav>
                 <Navigation />
